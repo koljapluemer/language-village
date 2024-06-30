2b56051474c635a0cb69e9dadd0d0aed031cc2f7
blackbrokkoli <kastomat@gmail.com>
2024-06-30 18:50:21 +0300

    make app show a prompt for a random common word

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <script setup>
-import words from "./words.js";
+import items from "@/assets/vocab.json";
 import { ref, computed } from "vue";
 
-const dividend = ref(0);
-const divisor = ref(1.09);
-const guess = ref(0);
-const results = ref([]);
 
-const homeCurrency = ref("EUR");
-const foreignCurrency = ref("USD");
-
-let unitsPracticedToday = 0;
-let unitsPracticedYesterday = 0;
-
-const items = {
-  أب: ["عب"],
-  "أبو سمبل": ["عبو سمبل", "أبو ثمبل", "أبو زمبل", "أبو صمبل"],
-};
-// same with localStorage stats
 let stats = {};
 if (!localStorage.getItem("stats")) {
   stats = {
@@ -27,59 +12,37 @@ if (!localStorage.getItem("stats")) {
   stats = JSON.parse(localStorage.getItem("stats"));
 }
 
-let isRevealed = ref(false);
+const isRevealed = ref(false);
+const item = ref(undefined);
 
 generateRandomExercise();
 
 async function generateRandomExercise() {
   isRevealed.value = false;
+  console.log('items', items["items"]);
   //  select random item from items array
-  const randomItem = Object.keys(items)[
-    Math.floor(Math.random() * Object.keys(items).length)
-  ];
+  item.value = items["items"][Math.floor(Math.random() * items["items"].length)];
+  console.log('selected', item.value);
 
   // TODO: need some way to handle secret
-  const response = await fetch(``);
-  const data = await response.json();
-  console.log(data);
 
 }
 </script>
 
 <template>
   <main class="">
-    <div class="card m-4 bg-gray-900">
+    <div class="card m-4 bg-gray-900" v-if="item">
       <div class="card-body">
-        <h2 class="card-title my-2 text-4xl">What is the correct spelling?</h2>
+        <h2 class="card-title my-2 text-2xl">Draw a fitting picture.</h2>
+        <h3 class="text-3xl my-2">{{item.EG}} — {{item.EN}}</h3>
 
-        <button class="btn btn-secondary mb-10">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-8 h-8"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-          />
-        </svg>
-        (Re)play word
-        </button>
+        <span class="uppercase">{{item.extra_info}}</span>
 
         <div class="grid grid-cols-2 gap-4">
-          <button class="btn btn-primary" @click="isRevealed = true">A</button>
+          <!-- <button class="btn btn-primary" @click="isRevealed = true">A</button>
           <button class="btn btn-primary" @click="isRevealed = true">B</button>
           <button class="btn btn-primary" @click="isRevealed = true">C</button>
-          <button class="btn btn-primary" @click="isRevealed = true">D</button>
+          <button class="btn btn-primary" @click="isRevealed = true">D</button> -->
         </div>
       </div>
     </div>
